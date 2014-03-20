b7da7827f59536430750eb98bfb98cc4e8247744
ndunnage <niel.dunnage@gmail.com>
2014-03-20 15:51:46 +0000

    Added a sample Azure Network configuration
    Create a sample network schema and added the values to the create VMs
    script

--- azure/CreateAzureVMs.sh
+++ azure/CreateAzureVMs.sh
@@ -3,12 +3,12 @@
 # This script will read in a list of hosts contained in hosts.txt
 # A passwordless Azure user is created for shell access
 hosts = "hosts.txt"
-VM_Network = <virtual-network-name>
+VM_Network = hadoop
 Subnet = Subnet-1
-Affintity_Group=<affinitygroup>
+Affintity_Group=hadoop-AG
 VM_Size = <instancesize>
 OSID = <OS id> ## eg b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-12_04_3-LTS-amd64-server-20140130-en-us-30GB
-Username = <username>
+Username = azureuser
 Password = 'Pa$$w0rd1'
 Ssh_Port = 22 # typically 22, a webservice end point is automatically created
 Ssh_Cert = <"ssh certificate location"> # eg /dir/MyCert.pem
